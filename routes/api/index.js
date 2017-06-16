fc36af4f5ac3fff695e6423764fe51eaca0c8ae6
jpogah <jpogah@gmail.com>
2017-06-15 23:16:54 -0500

    added a middleware error handler function to users.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,4 +2,23 @@ var router = require('express').Router();
 
 router.use('/',require('./users'));
 
+/*
+Below I create a middleware function for the API router
+to handle validation errors from Mongoose.This middleware
+converts the mongoose validation errors to something the
+front-end can consume
+*/
+
+router.use(function(err,req,res,next){ // middleware defined with 4 argument is treated as an error handler
+  if (err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors,key){
+        errors[key] = err.errors[key].message;
+        return errors;
+    },{})
+  });
+}
+return next(err);
+});
+
 module.exports = router;
